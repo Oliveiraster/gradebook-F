609f52750c63fc1e6f75233b9734f2871084a1fe
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2025-11-05 09:14:48 -0300

    chore: update ESLint configuration and add route for Gradebook page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { GradebookComponent } from './feature/gradebook/gradebook.component';
 
-export const routes: Routes = [{ path: '**', component: NotFoundComponent }];
+export const routes: Routes = [
+  { path: '', redirectTo: 'gradebook', pathMatch: 'full' },
+  { path: 'gradebook', component: GradebookComponent },
+  { path: '**', component: NotFoundComponent },
+];
