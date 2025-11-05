d1f32250da6cc557529f008e9ab722f355a2ea61
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2025-11-04 23:34:19 -0300

    feat: create NotFound page and configure route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '**', component: NotFoundComponent }];
